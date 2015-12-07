f3b9c9840e14b2ad7c8996627f6785843551ada4
A92hm <ali92hm@gmail.com>
2015-12-07 17:28:18 -0600

    claening

--- src/arena/light.cpp
+++ src/arena/light.cpp
@@ -5,9 +5,9 @@ const Enki::Color nevil::light::DEFAULT_ON_COLOR = Enki::Color(1.0, 1.0, 0.0);
 
 nevil::light::light()
   : nevil::object()
-  {}
+{}
 
 nevil::light::light(int x, int y, double size_x, double size_y
   , double height, const Enki::Color &off_color, const Enki::Color &on_color)
   : nevil::object(x, y, size_x, size_y, height, off_color, on_color)
-  {}
+{}
--- src/arena/wall.cpp
+++ src/arena/wall.cpp
@@ -2,9 +2,9 @@
 
 nevil::wall::wall()
   : nevil::object()
-  {}
+{}
 
 nevil::wall::wall(int x, int y, double size_x, double size_y
   , double height, const Enki::Color &color)
   : nevil::object(x, y, size_x, size_y, height, color, color)
-  {}
+{}
